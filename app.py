a57c5b50e9e49cb5b399ec348b9093090f9917b4
Tony <haichuan16888@126.com>
2020-04-10 22:12:40 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask, url_for, escape
+from flask import Flask, url_for, escape,  render_template
+
  
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 app = Flask(__name__)
- 
+
 #@app.route('/')
-#def hello_world():
-#    return 'Hello World!'
+#def hello():
+#    return 'Hello'
 
 @app.route('/')
-def hello():
-    return 'Hello'
-
+def index():
+    return render_template('index.html', name=name, movies=movies)
+"""
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % escape(name)
@@ -26,7 +41,7 @@ def test_url_for():
     print(url_for('test_url_for', num=2))  # 输出：/test?num=2
     return 'Test page'
 
-
+"""
 
 #if __name__ == '__main__':
 #    app.run(host='0.0.0.0', port=8888, debug=True)
