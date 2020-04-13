a48a11b150e9fa1b3f0e0d48d9f9ea33888d1d2a
Tony <haichuan16888@126.com>
2020-04-13 21:18:46 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -79,16 +79,21 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')  # 输出提示信息
 
-#@app.route('/')
-#def hello():
-#    return 'Hello'
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 @app.route('/')
 def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html', movies=movies)
 
 
 '''
